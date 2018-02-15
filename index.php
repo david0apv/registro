f7eb7e913c702a48d3e69e062d3082443b5068a1
david0apv <david0apv@gmail.com>
2018-02-15 08:17:22 -0600

    ADD:index.php,registro.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<head>
+<meta charset='UTF-8'>
+<title>Bienvenido</title>
+</head>
+<body>
+<h2>Bienvenido</h2>
+<h2>Descripciòn</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum libero auctor sodales vehicula. Nam porttitor fringilla mi at facilisis. Proin eros nunc, egestas eget condimentum id, elementum non sapien. Sed vitae massa eu arcu sagittis hendrerit. Pellentesque nec gravida mi. Maecenas viverra ac elit commodo placerat. Donec pretium commodo lectus ut aliquam. Donec a ultricies nulla, ac viverra nibh. Aenean id commodo turpis. Pellentesque et massa dolor. Ut in dapibus augue, non varius tortor.</p><br>
+<h2>Misiòn</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum libero auctor sodales vehicula. Nam porttitor fringilla mi at facilisis. Proin eros nunc, egestas eget condimentum id, elementum non sapien. Sed vitae massa eu arcu sagittis hendrerit. Pellentesque nec gravida mi. Maecenas viverra ac elit commodo placerat. Donec pretium commodo lectus ut aliquam. Donec a ultricies nulla, ac viverra nibh. Aenean id commodo turpis. Pellentesque et massa dolor. Ut in dapibus augue, non varius tortor.</p><br>
+<h2>Visiòn</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum libero auctor sodales vehicula. Nam porttitor fringilla mi at facilisis. Proin eros nunc, egestas eget condimentum id, elementum non sapien. Sed vitae massa eu arcu sagittis hendrerit. Pellentesque nec gravida mi. Maecenas viverra ac elit commodo placerat. Donec pretium commodo lectus ut aliquam. Donec a ultricies nulla, ac viverra nibh. Aenean id commodo turpis. Pellentesque et massa dolor. Ut in dapibus augue, non varius tortor.</p><br>
+<h2>Valores</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut bibendum libero auctor sodales vehicula. Nam porttitor fringilla mi at facilisis. Proin eros nunc, egestas eget condimentum id, elementum non sapien. Sed vitae massa eu arcu sagittis hendrerit. Pellentesque nec gravida mi. Maecenas viverra ac elit commodo placerat. Donec pretium commodo lectus ut aliquam. Donec a ultricies nulla, ac viverra nibh. Aenean id commodo turpis. Pellentesque et massa dolor. Ut in dapibus augue, non varius tortor.</p>
+</body>
---
+++ registro.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+<meta charset='UTF-8'>
+<title>Registro de usuarios</title>
+</head>
+<body>
+<h2>Registro de usuarios</h2>
+<form method="post">
+<p>Introduzca su nombre y apellidos</p>
+Nombre: <input type="text" name="nombre"><br>
+Apellido paterno: <input type="text" name="apaterno"><br>
+Apellido materno: <input type="text" name="amaterno"><br>
+<p>Introduzca su número telefónico</p>
+Telèfono: <input type="tel" name="telefono"><br>
+<p>Introduzca su correo electrónico</p>
+Correo electronico: <input type="mail" name="mail"><br>
+<input type="submit" value="Enviar">
+</form>
+</body>

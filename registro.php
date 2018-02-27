e1890f54bd028d8a45dcd1700c801590275e94f8
david0apv <david0apv@gmail.com>
2018-02-27 08:35:38 -0600

    ADD: registor.css

--- registro.php
+++ registro.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <head>
 <meta charset='UTF-8'>
+<link rel="stylesheet" type="text/css" href="css/registro.css">
 <title>Registro de usuarios</title>
 </head>
 <body>
 <h2>Registro de usuarios</h2>
+<div>
 <form method="post">
 <p>Introduzca su nombre y apellidos</p>
-Nombre: <input type="text" name="nombre"><br>
-Apellido paterno: <input type="text" name="apaterno"><br>
-Apellido materno: <input type="text" name="amaterno"><br>
+Nombre: <input type="text" name="nombre"><br><br>
+Apellido paterno: <input type="text" name="apaterno"><br><br>
+Apellido materno: <input type="text" name="amaterno"><br><br>
+</div>
+<div>
 <p>Introduzca su número telefónico</p>
-Telèfono: <input type="tel" name="telefono"><br>
+Telèfono: <input type="tel" name="telefono"><br><br>
+</div>
+<div>
 <p>Introduzca su correo electrónico</p>
-Correo electronico: <input type="mail" name="mail"><br>
+Correo electronico: <input type="mail" name="mail"><br><br>
 <input type="submit" value="Enviar">
 </form>
+</div>
 </body>

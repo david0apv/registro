09df63efd1d643ebc4e10ea8ee73d47014ce47a8
david0apv <david0apv@gmail.com>
2018-05-17 08:26:32 -0500

    ADD: Ejercicio primer AJAX

---
+++ primer_ajax.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<script src="js/jquery-3.3.1.js"></script>
+<script src="js/primer_ajax.js"></script>
+</head>
+<body>
+<div id="div1"><h2>Este texto sera cambiado con AJAX</h2></div>
+<button id="boton">Carga texto</button>
+</body>
+</html>
